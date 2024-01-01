27c7b6ff1cf19db4b7b4b65168554d0f8a2f17b1
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2023-12-31 21:10:29 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.js
+++ 0135-candy/0135-candy.js
@@ -8,7 +8,7 @@ var candy = function(ratings) {
     for (let i = 1; i < ratings.length; i++) {
         if (ratings[i] < ratings[i - 1]) {
             let j = i;
-            while (j > 0 && ratings[j] < ratings[j - 1] && candies[j] >= candies[j - 1]) {
+            while (j > 0 && ratings[j] < ratings[j - 1]) {
                 j--;
                 candies[j]++;
             }
