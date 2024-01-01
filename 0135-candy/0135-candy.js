98600531a80b56510c1cf9e4e14d0d007364cae5
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2023-12-31 22:22:22 -0500

    Time: 60 ms (72.53%), Space: 44.3 MB (89.07%) - LeetHub

--- 0135-candy/0135-candy.js
+++ 0135-candy/0135-candy.js
@@ -6,15 +6,14 @@ var candy = function(ratings) {
     const candies = Array.from({length: ratings.length}, () => 1);
 
     for (let i = 1; i < ratings.length; i++) {
-        if (ratings[i] < ratings[i - 1]) {
-            let j = i;
-            while (j > 0 && ratings[j] < ratings[j - 1]) {
-                j--;
-                candies[j]++;
-            }
-        }
-        else if (ratings[i] > ratings[i - 1]) {
+        if (ratings[i - 1] < ratings[i]) {
             candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    for (let i = ratings.length - 2; i > -1; i--) {
+        if (ratings[i + 1] < ratings[i] && candies[i + 1] >= candies[i]) {
+            candies[i] = candies[i + 1] + 1;
         }
     }
 
@@ -22,6 +21,5 @@ var candy = function(ratings) {
         return prevVal + newVal
     }, 0)
 
-    console.log(sum, candies);
     return sum;
 };
