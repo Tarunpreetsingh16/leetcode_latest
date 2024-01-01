8ec6d2f5dde5618958dec3726b5f089bdd2f0d52
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2023-12-31 22:26:06 -0500

    Time: 150 ms (9.58%), Space: 44.8 MB (81.14%) - LeetHub

--- 0135-candy/0135-candy.js
+++ 0135-candy/0135-candy.js
@@ -5,12 +5,14 @@
 var candy = function(ratings) {
     const candies = Array.from({length: ratings.length}, () => 1);
 
+    // first we need to assign candies to the children who have higher ratings than their left neighbor 
     for (let i = 1; i < ratings.length; i++) {
         if (ratings[i - 1] < ratings[i]) {
             candies[i] = candies[i - 1] + 1;
         }
     }
 
+    // now need to assign candies to the children who have higher ratings than their right neighbor 
     for (let i = ratings.length - 2; i > -1; i--) {
         if (ratings[i + 1] < ratings[i] && candies[i + 1] >= candies[i]) {
             candies[i] = candies[i + 1] + 1;
