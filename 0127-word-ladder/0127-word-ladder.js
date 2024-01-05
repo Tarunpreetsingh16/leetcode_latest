3287cb3090364d683155c259c39b6418d8cbad06
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2024-01-05 09:01:27 -0500

    Time: 1101 ms (18.03%), Space: 49.7 MB (84.39%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.js
+++ 0127-word-ladder/0127-word-ladder.js
@@ -10,57 +10,32 @@ const CustomNode = function(word, edges) {
 }
 
 var ladderLength = function(beginWord, endWord, wordList) {
-    let result = Infinity;
-    // wordList.push(beginWord);
+    let result = 0;
     if (beginWord != endWord) {
         const visited1 = new Map();
-        const visited2 = new Map();
 
         const queue1 = new Queue();
         queue1.enqueue(new CustomNode(beginWord, 1));
-
-        // const queue2 = new Queue();
-        // if (wordList.includes(endWord)) queue2.enqueue(new CustomNode(endWord, 0));
-
-        // while (!queue1.isEmpty() && !queue2.isEmpty()) {
         while (!queue1.isEmpty()) {
-            let node = queue1.dequeue();
-            let word = node.word;
-            let edges = node.edges;
+            let {word, edges} = queue1.dequeue();
             if (!visited1.has(word)) {
                 visited1.set(word, edges);
                 
-                // if (visited2.has(word)) {
                 if (word === endWord) {
                     result = edges;
                 } 
                 else {
                     const validTransformations = getValidTransformations(word, wordList);
-                    const unvisitedValidTransformations = getUnvisitedValidTransformations(validTransformations, visited1);
+                    const unvisitedValidTransformations = getUnvisitedValidTransformations(
+                        validTransformations, 
+                        visited1);
                     enqueueTransformatons(queue1, unvisitedValidTransformations, edges);
                 }
             }
-
-            // node = queue2.dequeue();
-            // word = node.word;
-            // edges = node.edges;
-            // if (!visited2.has(word)) {
-            //     visited2.set(word, edges);
-                
-            //     if (visited1.has(word)) {
-            //         console.log(result, "there")
-            //         result = Math.min(visited1.get(word) + edges + 1, result);
-            //     } 
-            //     else {
-            //         const validTransformations = getValidTransformations(word, wordList);
-            //         const unvisitedValidTransformations = getUnvisitedValidTransformations(validTransformations, visited2);
-            //         enqueueTransformatons(queue2, unvisitedValidTransformations, edges);
-            //     }
-            // }
         }
 
     }
-    return result === Infinity ? 0 : result;
+    return result;
 };
 
 const getValidTransformations = (word, wordList) => {
