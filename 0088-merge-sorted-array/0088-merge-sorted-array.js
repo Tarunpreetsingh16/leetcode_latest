533fb4bb6d3bd690faa52f725c7e89df4899a912
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2024-01-11 09:09:43 -0500

    Time: 58 ms (31.26%), Space: 42.1 MB (34.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -8,20 +8,10 @@
 var merge = function(nums1, m, nums2, n) {
     let i = m - 1, j = n - 1, k = m + n - 1;
 
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] <= nums2[j]) {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        else {
-            nums1[k] = nums1[i];
-            i--;
-        }
-        k--;
-    }
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        k--;
-        j--;
-    }
+    while (i >= 0 && j >= 0) 
+        nums1[k--] = nums1[i] <= nums2[j] ?
+            nums2[j--] : 
+            nums1[i--];
+    while (j >= 0) 
+        nums1[k--] = nums2[j--];
 };
