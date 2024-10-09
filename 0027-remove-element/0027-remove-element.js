f753bbcd9b068a41f900b7a8f8e1d8b93caabdb8
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2024-10-09 00:05:51 -0400

    Time: 60 ms (20.68%), Space: 49.3 MB (18.95%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,29 +4,18 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let l = 0, r = nums.length - 1;
-    while (l < r) {
-        if (nums[r] === val) {
-            r--;
-            continue;
-        }
-        if (nums[l] != val) l++;
-        else if (nums[l] === val) {
-            nums[l] = nums[r] + nums[l];
-            nums[r] = nums[l] - nums[r];
-            nums[l] = nums[l] - nums[r];
-            r--;
-            l++;
-        }
-    }
-    let result = 0;
-    nums.some(num => {
+    let k = 0;
+    let left = 0;
+    let right = 0;
+
+    while (right < nums.length) {
+        const num = nums[right];
         if (num != val) {
-            result++
+            nums[left] = num;
+            left++;
+            k++;
         }
-        else {
-            return true;
-        }
-    });
-    return result;
+        right++;
+    }
+    return k;
 };
