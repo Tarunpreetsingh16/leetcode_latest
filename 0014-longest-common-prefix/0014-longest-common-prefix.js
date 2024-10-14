914744b24a4420f39ec22b8bb6482f333429b711
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2024-10-14 12:08:58 -0400

    Time: 56 ms (53.3%), Space: 43.9 MB (100%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -1,55 +1,60 @@
-function TrieNode(val) {
-    this.val = val;
-    this.children = new Map();
-    this.wordEnd = false;
-
-    this.addWord = function(word) {
-        if (!word || word.length === 0) return;
-
-        const firstChar = word[0];
-        let nextCharNode = this.getNextCharNode(firstChar);
-        
-        if (!nextCharNode) {
-            const newNode = new TrieNode(firstChar);
-            this.children.set(firstChar, newNode);
-            nextCharNode = newNode;
-        }
-        nextCharNode.addWord(word.substring(1));
-    }
-
-    this.getNextCharNode = function(char) {
-        return this.children.get(char);
-    }
-}
-
-
-function Trie() {
-    return new TrieNode(null);
-}
-
 /**
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    const root = new Trie();
-    let minLength = Infinity;
-    strs.forEach(str => {
-        root.addWord(str)
-        minLength = Math.min(minLength, str.length);
-    });
 
-    let result = "";
-    let size = root.children.size;
-    let node = root;
-    while (size === 1 && minLength > 0) {
-        const keys = node.children.keys();
-        const nextChar = keys.next().value;
-        node = node.children.get(nextChar);
-        result += nextChar;
-        size = node.children.size;
-        minLength--;
+
+function TrieNode(val) {
+    this.val = val
+    this.terminates = false
+    this.children = {}
+    
+    this.addWord = function(word) {
+        if (!word || word.length === 0) return null
+        
+        const firstChar = word[0]
+        var child = this.getChild(firstChar)
+        if (!child) {
+            child = new TrieNode(firstChar)
+            this.children[firstChar] = child
+        }
+        if (word.length > 1) {
+            child.addWord(word.substring(1))
+        }
     }
 
-    return result;
+    this.getChild = function(char) {
+        return this.children[char]
+    }
+} 
+
+function Trie() {
+    return new TrieNode(null)
+}
+var longestCommonPrefix = function(strs) {
+    const trie = Trie()
+    const emptyStringFound = strs.some(str => {
+        const response = trie.addWord(str)
+        return response === null
+    })
+
+    var longestPrefix = ""
+
+    if (!emptyStringFound) {
+
+        var trieNode = trie
+
+        while (Object.keys(trieNode.children).length === 1) {
+            trieNode = trieNode.children[Object.keys(trieNode.children)[0]]
+            longestPrefix = longestPrefix + trieNode.val
+        }
+    }
+
+    var shortestStringLength = 999
+    strs.forEach(str => {
+        if (str.length < shortestStringLength) shortestStringLength = str.length
+    })
+
+    return longestPrefix.substring(0, shortestStringLength)
+
 };
