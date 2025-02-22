b26124651ef62b354e624926e483f7ffd94f4f9a
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2025-02-22 16:15:56 -0500

    Time: 4 ms (73.7%), Space: 57.9 MB (11.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,15 +3,11 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const cleanedString = s.replace(/[^a-z0-9]/ig, "").toLowerCase();
-    if (cleanedString.length === 0) return true;
-
-    let left = 0; right = cleanedString.length - 1;
-    while (left < right) {
-        if (cleanedString[left] != cleanedString[right]) {
-            return false;
-        }
-        left++; right--;
+    const cleanString = s.replace(/[^a-z0-9]/ig, "").toLowerCase()
+    if (cleanString.trim().length === 0) return true
+    let l = 0, r = cleanString.length - 1
+    while (l <= r && cleanString[l] === cleanString[r]) {
+        l++, r--
     }
-    return true;
+    return r < l
 };
