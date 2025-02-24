bead4d9d2ea75fe9f34eeb2ccf76dfe560d22d52
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2025-02-23 20:45:13 -0500

    Time: 9 ms (40.13%), Space: 56.7 MB (5.52%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,20 +4,20 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    const map1 = {}
-    const map2 = {}
+    if (s.length != t.length) return false
+    let i = 0, mapForS = new Map(), mapForT = new Map()
 
-    var isNotIsomorphic = false
-    for (var i = 0; i < s.length; i++) {
-        if (
-            (s[i] in map1 && map1[s[i]] != t[i]) || 
-            (t[i] in map2 && map2[t[i]] != s[i])
-        ) {
-            isNotIsomorphic = true;
-            break;
+    while (i < s.length) {
+        if (mapForS.has(s[i]) || mapForT.has(t[i])) {
+            if (!(mapForS.get(s[i]) === t[i] && mapForT.get(t[i]) === s[i])) {
+                return false
+            }
         }
-        map2[t[i]] = s[i]
-        map1[s[i]] = t[i]
+        else {
+            mapForS.set(s[i], t[i])
+            mapForT.set(t[i], s[i])
+        }
+        i++
     }
-    return !isNotIsomorphic
+    return true
 };
