935dbb381c4f12a554de5f981a9e8f05b72ffeda
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2025-02-23 19:59:08 -0500

    Time: 19 ms (57.09%), Space: 59.1 MB (5.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,26 +4,33 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const map = new Map();
+    if (ransomNote.length > magazine.length) return false
+
+    const map = new Map()
     magazine.split('').forEach(char => {
-        if (!map.has(char)) {
-            map.set(char, 0);
+        let count = 1
+        if (map.has(char)) {
+            count = map.get(char) + 1
         }
-        const currentCount = map.get(char)
-        map.set(char, currentCount + 1)
+        map.set(char, count)
     })
 
-    let i = 0;
-    while (i < ransomNote.length) {
-        const char = ransomNote[i];
-
-        if (map.has(char) && map.get(char) > 0) {
-            const currentCount = map.get(char)
-            map.set(char, currentCount - 1);
-            i++;
-            continue;
+    let found = false
+    for (let i = 0; i < ransomNote.length; i++) {
+        const char = ransomNote[i]
+        if (map.has(char)) {
+            const count = map.get(char)
+            map.set(char, count - 1)
+            if (count - 1 === 0) {
+                map.delete(char)
+            }
+            if (i === ransomNote.length - 1) {
+                found = true
+            }
         }
-        return false;
+        else {
+            break
+        }
     }
-    return true;
+    return found
 };
