cdbfd9131fecc1a79228d5a69de2042fd97138a8
Tarunpreet Singh <Tarunpreetsingh16@gmail.com>
2025-02-27 14:23:25 -0500

    Time: 0 ms (100%), Space: 56.2 MB (14.36%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,17 +11,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (!root) return 0;
-
-    const findMaxDepth = (node, depth) => {
-        if (!node) {
-            return depth - 1;
-        }
-        return Math.max(
-            depth, 
-            findMaxDepth(node.left, depth + 1), 
-            findMaxDepth(node.right, depth + 1))
-    }
-
-    return findMaxDepth(root, 1);
-};
+    if (!root) return 0
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
